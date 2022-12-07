604fbf731f5e6078b5a01ba548e2448e0b6e051a
Eric Leonardo <ericl619@gmail.com>
2022-12-07 18:16:18 +0800

    commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,9 +6,6 @@
   let { posts } = data
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
 <Head title={`All the Posts`} />
 
 <div class="flex flex-col flex-grow">
