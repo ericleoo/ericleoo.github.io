ecf70277e96fbc1f444f9f4bbe2da6b49c927d6c
Eric Leonardo <ericl619@gmail.com>
2022-12-08 10:06:53 +0800

    support auto-hide navbar

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -1,11 +1,44 @@
 <script>
   import { pages, siteName } from '@lib/site-config'
+  let duration = "350ms";
+  let offset = 50;
+  let tolerance = 5;
+
+  let headerClass = "show";
+  let y = 0;
+  let lastY = 0;
+
+  function deriveClass(y, dy) {
+    if (y < offset) {
+      return "show";
+    }
+
+    if (Math.abs(dy) <= tolerance) {
+      return headerClass;
+    }
+
+    if (dy < 0) {
+      return "hide";
+    }
+
+    return "show";
+  }
+
+  function updateClass(y) {
+    const dy = lastY - y;
+    lastY = y;
+    return deriveClass(y, dy);
+  }
+
+  function setTransitionDuration(node) {
+    node.style.transitionDuration = duration;
+  }
+
+  $: headerClass = updateClass(y);
 </script>
 
-<div
-  class="bg-neutral shadow-lg text-neutral-content mb-16 top-0 z-10 navbar sticky"
->
-  <!--
+<div use:setTransitionDuration class="bg-neutral shadow-lg text-neutral-content mb-16 top-0 z-10 navbar sticky {headerClass}">
+  <!-- 
   <div class="mx-2 px-2 navbar-start left">
     <span class="font-bold text-lg">{siteName}</span>
   </div>
@@ -15,8 +48,7 @@
     <div tabindex="0" class="m-1 btn">Links</div>
     <ul
       tabindex="0"
-      class="bg-neutral rounded-box shadow text-neutral-content p-2 w-52 menu dropdown-content "
-    >
+      class="bg-neutral rounded-box shadow text-neutral-content p-2 w-52 menu dropdown-content ">
       {#each pages as { title, path }}
         <li>
           <a sveltekit:prefetch href={path}>{title}</a>
@@ -24,8 +56,8 @@
       {/each}
     </ul>
   </div>
-  !-->
-  <div class="mx-2 px-2 hidden navbar-center lg:flex right">
+  -->
+  <div class="mx-2 px-2 navbar-center lg:flex right">
     <div class="flex items-stretch">
       {#each pages as { title, path }}
         <a
@@ -38,3 +70,14 @@
     </div>
   </div>
 </div>
+
+<svelte:window bind:scrollY={y} />
+
+<style>
+  .show {
+    transform: translateY(0%);
+  }
+  .hide {
+    transform: translateY(-100%);
+  }
+</style>
